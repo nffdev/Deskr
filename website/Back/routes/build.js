21b4afaede992a7de3e5205faca28dcbd5c62818
nffdev <nightfrost2discord@outlook.fr>
2026-03-30 01:22:36 +0200

    feat(back): updated build routes

--- website/Back/routes/build.js
+++ website/Back/routes/build.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const build = require('../controllers/build');
 
 router.post('/', build.startBuild);
+router.post('/upload/icon', build.uploadIcon);
 router.get('/download', build.downloadBuild);
 
 module.exports = router;
