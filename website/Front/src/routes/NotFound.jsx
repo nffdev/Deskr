029c0fff633b53399688da67755e28f0217e96cb
nffdev <nightfrost2discord@outlook.fr>
2026-03-31 01:09:44 +0200

    feat(front): updated responsive for notfound page

--- website/Front/src/routes/NotFound.jsx
+++ website/Front/src/routes/NotFound.jsx
@@ -5,10 +5,10 @@ export default function NotFound() {
     const navigate = useNavigate();
 
     return (
-        <div className="flex flex-col gap-6 items-center justify-center w-full h-screen bg-gray-800 text-white">
-            <h1 className="text-8xl font-extrabold">404</h1>
-            <p className="text-xl text-muted-foreground">Cette page n'existe pas.</p>
-            <Button onClick={() => navigate('/')}>Retour à l'accueil</Button>
+        <div className="flex flex-col gap-6 items-center justify-center w-full h-screen px-6 bg-gray-800 text-white">
+            <h1 className="text-6xl sm:text-8xl font-extrabold">404</h1>
+            <p className="text-lg sm:text-xl text-muted-foreground text-center">Cette page n'existe pas.</p>
+            <Button className="w-full sm:w-auto px-8" onClick={() => navigate('/')}>Retour à l'accueil</Button>
         </div>
     );
 }
