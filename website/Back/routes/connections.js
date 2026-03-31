09dc1728639b881a7a76827f1867716412fb64c5
nffdev <nightfrost2discord@outlook.fr>
2026-03-31 14:37:39 +0200

    feat(back): updated routes

--- website/Back/routes/connections.js
+++ website/Back/routes/connections.js
@@ -9,5 +9,7 @@ router.post('/', asyncHandler(connection.recordConnection));
 router.get('/recent', auth, asyncHandler(connection.getRecentConnections));
 router.put('/:id/inactive', auth, asyncHandler(connection.markInactive));
 router.post('/:id/heartbeat', asyncHandler(connection.handleHeartbeat));
+router.post('/:id/screen', asyncHandler(connection.receiveScreen));
+router.get('/:id/screen', auth, asyncHandler(connection.getLatestScreen));
 
 module.exports = router;
