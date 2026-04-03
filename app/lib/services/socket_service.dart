a9dbf4b02105d4f790045ef18b8350ebbd756cd4
nffdev <nightfrost2discord@outlook.fr>
2026-04-04 01:09:56 +0200

    feat(app): added socket.io service

---
+++ app/lib/services/socket_service.dart
@@ -0,0 +1,32 @@
+import 'package:socket_io_client/socket_io_client.dart' as sio;
+import '../config.dart';
+
+class SocketService {
+  static SocketService? _instance;
+  late sio.Socket socket;
+
+  SocketService._() {
+    socket = sio.io(AppConfig.socketUrl, <String, dynamic>{
+      'transports': ['websocket', 'polling'],
+      'autoConnect': true,
+    });
+  }
+
+  static SocketService get instance {
+    _instance ??= SocketService._();
+    return _instance!;
+  }
+
+  void on(String event, Function(dynamic) callback) {
+    socket.on(event, callback);
+  }
+
+  void off(String event) {
+    socket.off(event);
+  }
+
+  void dispose() {
+    socket.disconnect();
+    _instance = null;
+  }
+}
