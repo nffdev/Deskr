096ba11b4d4b5aa347c039c86037dd715d703d9a
nffdev <nightfrost2discord@outlook.fr>
2026-04-08 23:10:00 +0200

    feat(website): updated user model

--- website/Back/models/User.js
+++ website/Back/models/User.js
@@ -4,7 +4,11 @@ const userSchema = new Schema({
     id: String,
     username: String,
     email: String,
-    password: String
+    password: String,
+    notifications: {
+        buildNotifications: { type: Boolean, default: true },
+        connectionAlerts: { type: Boolean, default: true }
+    }
 });
 
 module.exports = model('user', userSchema);
