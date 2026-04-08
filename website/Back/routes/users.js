f87467ad31bf1cc14f1ce1e1c3162f876afe2cf3
nffdev <nightfrost2discord@outlook.fr>
2026-04-08 23:10:19 +0200

    feat(website): updated user routes

--- website/Back/routes/users.js
+++ website/Back/routes/users.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getMe } = require('../controllers/users');
+const { getMe, changePassword, updateAccount, updateNotifications, getStorage, clearStorage } = require('../controllers/users');
 const authMiddleware = require('../middleware/auth');
 const { asyncHandler } = require('../utils');
 
 router.get('/@me', authMiddleware, asyncHandler(getMe));
+router.put('/password', authMiddleware, asyncHandler(changePassword));
+router.put('/account', authMiddleware, asyncHandler(updateAccount));
+router.put('/notifications', authMiddleware, asyncHandler(updateNotifications));
+router.get('/storage', authMiddleware, asyncHandler(getStorage));
+router.delete('/storage', authMiddleware, asyncHandler(clearStorage));
 
 module.exports = router;
