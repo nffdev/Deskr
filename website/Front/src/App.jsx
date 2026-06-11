d82a7c01376a349ec6afba8679714cf3f69c9347
nffdev <nightfrost2discord@outlook.fr>
2026-06-11 14:53:52 +0200

    feat(website): added loader

--- website/Front/src/App.jsx
+++ website/Front/src/App.jsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthWrapper } from "@/lib/hooks/useAuth";
 import { UserContextProvider } from "@/lib/contexts/userContext";
 import useRouter from "@/lib/hooks/useRouter";
 import NotFound from "@/routes/NotFound";
 import { Toaster } from "@/components/ui/sonner";
+import { FullPageLoader } from "@/components/ui/loader";
 
 export default function App() {
   const routes = useRouter();
@@ -12,12 +14,14 @@ export default function App() {
     <BrowserRouter>
       <UserContextProvider>
         <AuthWrapper>
-          <Routes>
-            {routes.map(({ path, component: Component }) => (
-              <Route key={path} path={path} element={<Component />} />
-            ))}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<FullPageLoader />}>
+            <Routes>
+              {routes.map(({ path, component: Component }) => (
+                <Route key={path} path={path} element={<Component />} />
+              ))}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </AuthWrapper>
       </UserContextProvider>
       <Toaster position="top-right" />
