a46713d68485e01607e731dcb9928d7015750aae
nffdev <nightfrost2discord@outlook.fr>
2026-06-11 16:43:20 +0200

    fix(website): fixed CORS to allow DELETE method

--- website/Back/index.js
+++ website/Back/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: (origin, cb) => cb(null, origin || true),
     credentials: true,
-    methods: ['GET', 'POST', 'PUT']
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 const { globalLimiter } = require('./middleware/rateLimiter');
