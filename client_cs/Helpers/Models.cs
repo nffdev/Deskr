f3b131a4aeb2ce0f0f1241e1aa05b40b279449db
nffdev <nightfrost2discord@outlook.fr>
2026-06-28 16:24:22 +0200

    feat(client_cs): added ownerId to ConnectionRequest and MonitorInfo model

--- client_cs/Helpers/Models.cs
+++ client_cs/Helpers/Models.cs
@@ -28,6 +28,33 @@ namespace client.Models
 
         [JsonPropertyName("deviceInfo")]
         public string DeviceInfo { get; set; }
+
+        [JsonPropertyName("ownerId")]
+        public string OwnerId { get; set; }
+    }
+
+    public class MonitorInfo
+    {
+        [JsonPropertyName("index")]
+        public int Index { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("x")]
+        public int X { get; set; }
+
+        [JsonPropertyName("y")]
+        public int Y { get; set; }
+
+        [JsonPropertyName("width")]
+        public int Width { get; set; }
+
+        [JsonPropertyName("height")]
+        public int Height { get; set; }
+
+        [JsonPropertyName("isPrimary")]
+        public bool IsPrimary { get; set; }
     }
 
     public class IpApiResponse
